12eeaa1b55a956d8750e9f777495237077e98367
Eric Sipple <esipple@stitchfix.com>
2018-12-02 15:31:12 -0800

    removing unnecessary provider import.

--- app/javascript/landing/components/landing/applicationStatus.jsx
+++ app/javascript/landing/components/landing/applicationStatus.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import {Provider} from 'react-redux'
 import moment from 'moment'
 
 export default class ApplicationStatus extends React.Component {
-
-
 
   render() {
     return(
