b6ef74e43276588eca204709f4fd485705e56218
Eric Sipple <esipple@stitchfix.com>
2019-11-11 14:48:49 -0500

    Hopefully fixing tests and deprecating old ones.

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,3 +1,9 @@
 const environment = require('./environment')
 
 module.exports = environment.toWebpackConfig()
+
+describe('useless', function(){
+  test('why does this need to be here', function() {
+    expect(1).toBe(1)
+  })
+});
