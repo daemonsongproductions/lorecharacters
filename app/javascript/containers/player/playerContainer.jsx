d874b0da9c67cc19d8e8441952cb2d661eca4724
Eric Sipple <esipple@stitchfix.com>
2019-11-12 10:15:01 -0500

    Removing redux from player component.

--- app/javascript/containers/player/playerContainer.jsx
+++ app/javascript/containers/player/playerContainer.jsx
@@ -1,37 +1,11 @@
 import React from "react"
-import {connect} from "react-redux"
-import {fetchAccountInfo} from '../../shared/actions'
 import Profile from './components/profile'
-
-const mapStateToProps = (
-    state
-) => {
-  return {
-    ...state
-  }
-};
-
-const mapDispatchToProps = (
-    dispatch
-) => {
-  return {
-    fetchPlayer: () => {
-      dispatch(fetchAccountInfo())
-    }
-  }
-};
-
-const ProfileComponent = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Profile);
-
 
 
 export default class PlayerContainer extends React.Component {
   render () {
     return(
-        <ProfileComponent/>
+        <Profile/>
     )
   }
 }
