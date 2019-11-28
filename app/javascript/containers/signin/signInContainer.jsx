3f6972dd8ffe64c569f34b8a8f9233cf3416e0dc
Eric Sipple <esipple@stitchfix.com>
2019-11-28 13:42:10 -0500

    removing redux since we are no longer using it.

--- app/javascript/containers/signin/signInContainer.jsx
+++ app/javascript/containers/signin/signInContainer.jsx
@@ -1,7 +1,4 @@
 import React from "react"
-import {connect} from "react-redux"
-import {signIn, switchForms, submitRegistration} from './actions'
-import {handleFormChange} from '../../shared/actions'
 import {Container} from 'reactstrap';
 import AccountForms from './components/accountForms'
 
